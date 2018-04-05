266ca82b43eceb357508ee3364946cf95b122a98
Pavan <pavanreddy301@achiever-99.local>
2018-04-05 16:42:32 +0530

    first edit

---
+++ as/as/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  as
+//
+//  Created by Pavan on 4/5/18.
+//  Copyright © 2018 Pavan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("hai")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
